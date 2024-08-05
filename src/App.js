699f58b5dedc964807d5491b62ee766c7c96503f
Ishi Rastogi <ishi.rastogi@icloud.com>
2024-08-05 11:34:08 +0530

    changes mortgagecalculator

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Start from "./components/Start";
+import MortgageCalculator from "./components/MortgageCalculator";
+
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/start" element={<Start />} />
+          <Route path="/mortgage-calculator" element={<MortgageCalculator />} />
         </Routes>
       </Router>
       <Footer />
